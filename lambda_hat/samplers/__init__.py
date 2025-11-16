5ff24d1cd04bf55967b48cd8412e1d5778247d4e
dan mackinlay <dan@livingthing.org>
2025-11-16 23:17:51 +0800

    Remove run_sgld_basic from __init__.py
    Function was deleted in earlier refactor but import not removed

--- lambda_hat/samplers/__init__.py
+++ lambda_hat/samplers/__init__.py
@@ -3,13 +3,12 @@
 
 from lambda_hat.samplers.hmc import run_hmc
 from lambda_hat.samplers.mclmc import run_mclmc
-from lambda_hat.samplers.sgld import run_sgld, run_sgld_basic
+from lambda_hat.samplers.sgld import run_sgld
 from lambda_hat.samplers.vi import run_vi
 
 __all__ = [
     "run_hmc",
     "run_mclmc",
     "run_sgld",
-    "run_sgld_basic",
     "run_vi",
 ]
